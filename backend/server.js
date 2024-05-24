e26c1b77ea2e43e9982730d99902b04b4f6327af
MOHIT KUMAR SHAW <97303625+mohitshaw01@users.noreply.github.com>
2024-05-25 00:36:23 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,27 @@ import express from 'express';
 import env from 'dotenv';
 import cors from 'cors';
 import connectToDB from './db/dbConnection.js';
-// morgan is a HTTP request logger middleware for node.js
 import morgan from "morgan";
 import path from 'path';
+import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
-import {app,server} from './socket/socket.js';
-//
-// const app = express();
-env.config();
-app.use(cors({
-    origin : "http://localhost:3000",
-    credentials : true,
-}))
+import { app, server } from './socket/socket.js';
 
-app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
-app.use(express.json()); // for parsing application/json payloads
-// morgan is a HTTP request logger middleware for node.js
+env.config();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev")); //HTTP request logger middleware for node.js that logs requests to the console
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     res.send("Server is active");
 });
-
 
 // Routes
 import authRoutes from './routes/auth.routes.js';
@@ -33,18 +31,22 @@ import userRoutes from './routes/user.routes.js';
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
-// serve static assets if in production
+
+// Handling __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static assets if in production
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-
 server.listen(process.env.PORT, () => {
-	connectToDB()
-    .then(() => {
-        console.log(`Server is listening on: ${process.env.PORT}`);
-    })
-    .catch((error) => console.log("MONGODB connection failed!!!: ", error));
+    connectToDB()
+        .then(() => {
+            console.log(`Server is listening on: ${process.env.PORT}`);
+        })
+        .catch((error) => console.log("MONGODB connection failed!!!: ", error));
 });
